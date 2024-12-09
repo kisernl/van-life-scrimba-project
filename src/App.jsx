2bd96dfcbe624f601dbea8dd79d3598a83b806dc
kisernl <noah.kiser@gmail.com>
2024-12-08 20:49:16 -0600

    add vans page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
+import Vans from './pages/Vans';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import "./server";
+
+/**
+ * Challenge: Create the Vans list page, Route, and Link
+ * 
+ * 1. Create a Vans component in the "pages" directory. For now,
+ *    just render <h1>Vans page goes here 🚐</h1>
+ * 2. Create a Route for the Vans page on the /vans route
+ * 3. Add a Link in the nav bar to the Vans route
+ */
 
 function App() {
   return (
@@ -12,6 +23,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
         </Routes>
       </BrowserRouter>
     </>
